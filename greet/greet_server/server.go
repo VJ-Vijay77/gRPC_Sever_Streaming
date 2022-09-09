a273ad34a94aca8e70579cee15abc57aeed36f3b
VJ-Vijay77 <vijaydinesh77vj@gmail.com>
2022-09-09 15:32:43 +0530

    server streaming grpc

---
+++ greet/greet_server/server.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"strconv"
+	"time"
+
+	"github.com/VJ-Vijay77/gRPC/greet/greetpb"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{
+
+}
+
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	fmt.Printf("Greet function was invoked with %v \n",req)
+	firstName := req.GetGreeting().GetFirstName()
+	result := "hello " + firstName
+
+	res := greetpb.GreetResponse{
+		Result: result,
+	}
+
+	return &res,nil
+
+}
+
+func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest,stream greetpb.GreetService_GreetManyTimesServer) error {
+	firstName := req.GetGreeting().GetFirstName()
+	
+	for i := 0 ; i<10; i++ {
+		result := "Hello "+firstName +" number"+strconv.Itoa(i)
+		res := &greetpb.GreetManyTimesResponse{
+			Result: result,
+		}
+		stream.Send(res)
+		time.Sleep(1000 *time.Millisecond)
+		return nil
+	}
+}
+
+
+
+func main() {
+	fmt.Println("Starting")
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalln(err)
+	}
+
+}
